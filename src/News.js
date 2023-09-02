5c695a6f833e3dd9bc09855a2451c89b1d1c4724
mrityunjay-pandey <140483827+mrityunjay-pandey@users.noreply.github.com>
2023-09-02 10:13:01 +0530

    video number 26 completed along with practice but image of the news is not showing

--- src/News.js
+++ src/News.js
@@ -43,15 +43,11 @@ export class News extends Component {
        <div className="container my-3">
             <h2>NewsMonkey - Top Headlines</h2>
             <div className="row">
-              <div className="col-md-4">
-                <NewsItem title="myTitle" description="mydesc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" newsUrl = "TODO"/>
-            </div>
-              <div className="col-md-4">
-                <NewsItem title="myTitle" description="mydesc"/>
-            </div>
-              <div className="col-md-4">
-                <NewsItem title="myTitle" description="mydesc"/>
-            </div>
+            {this.state.articles.map((element)=>{
+              return  <div className="col-md-4" key={element.url}>
+              <NewsItem  title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl = {element.url}/>
+            </div>  
+            })}
             </div>
        </div>
         )
--- src/NewsItem.js
+++ src/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-        <img className="card-img-top" src="imageUrl" alt="Card image cap"/>
+        <img className="card-img-top" src="imageUrl" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
         </div>
 </div>
       </div>
