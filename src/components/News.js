5799698db61f9845275707fcfb749001328111a8
mrityunjay-pandey <140483827+mrityunjay-pandey@users.noreply.github.com>
2023-09-04 17:33:18 +0530

    video number 30 completed along with practice

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+  static defaultProps = {
+    country: 'in',
+    pageSize:8,
+    category:'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
   constructor(){
     super();
     console.log("Hello I am a constructor from News component");
@@ -14,7 +26,7 @@ export class News extends Component {
   }
 
   async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json()
@@ -27,7 +39,7 @@ export class News extends Component {
 
    handlePrevClick= async()=>{
     console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json()
@@ -42,7 +54,7 @@ export class News extends Component {
    handleNextClick= async ()=>{
       console.log("Next");
       if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       this.setState({loading: true});
       let data = await fetch(url);
       let parsedData = await data.json()
@@ -58,13 +70,12 @@ export class News extends Component {
     render() {
       return (
        <div className="container my-3">
-        <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+        <h1 className="text-center" style={{margin:'35px 0px'}}>NewsMonkey - Top Headlines</h1>
         {this.state.loading && <Spinner/>}
             <div className="row">
             {!this.state.loading && this.state.articles.map((element)=>{
               return  <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title?element.title.slice(0,45):""} description={element.
-                description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl = {element.url}/>
+              <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl = {element.url}/>
             </div>  
             })}
             </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
       let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
         <img src={!imageUrl?"https://www.bing.com/images/search?view=detailv2&form=SBIHVR&iss=sbi&q=imgurl:https%3A%2F%2Fstatic.toiimg.com%2Fthumb%2Fmsid-103291540%2Cimgsize-64350%2Cwidth-400%2Cresizemode-4%2F103291540.jpg&pageurl=https%3A%2F%2Ftimesofindia.indiatimes.com%2Fsports%2Fcricket%2Fasia-cup%2Findia-vs-pakistan-asia-cup-virat-kohli-vs-shaheen-afridi-and-other-player-battles-to-watch-out-for%2Farticleshow%2F103291344.cms&pagetl=India+vs+Pakistan%2C+Asia+Cup%3A+Virat+Kohli+vs+Shaheen+Afridi+and+other+player+battles+to+watch+out+for+%7C+Cricket+News+-+Times+of+India&imgalt=India+vs+Pakistan%2C+Asia+Cup%3A+Virat+Kohli+vs+Shaheen+Afridi+and+other+player+battles+to+watch+out+for&imgsz=195x111&selectedindex=0&id=https%3A%2F%2Fstatic.toiimg.com%2Fthumb%2Fmsid-103291540%2Cimgsize-64350%2Cwidth-400%2Cresizemode-4%2F103291540.jpg&ccid=qOifeYqP&simid=6955629249185&ck=51F8C6667EB2D065E658309EC65FEFE8&thid=OIF.UfjGZn6y0GXmWDCexl%2Fv6A&mediaurl=https%3A%2F%2Fstatic.toiimg.com%2Fthumb%2Fmsid-103291540%2Cimgsize-64350%2Cwidth-400%2Cresizemode-4%2F103291540.jpg&exph=225&expw=400&vt=2&sim=1":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
