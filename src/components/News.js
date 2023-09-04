32a9d4a22344239eb6f0c8d196b7a2ea83941604
mrityunjay-pandey <140483827+mrityunjay-pandey@users.noreply.github.com>
2023-09-04 23:18:26 +0530

    video number 33 completed along with practice

--- src/components/News.js
+++ src/components/News.js
@@ -25,46 +25,52 @@ export class News extends Component {
     }
   }
 
-  async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  async updateNews(){
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json()
-    console.log(parsedData);
     this.setState({articles:parsedData.articles,
        totalResults: parsedData.totalResults,
       loading: false})
+  }
 
+  async componentDidMount(){
+  this.updateNews();
   }
 
    handlePrevClick= async()=>{
-    console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false
-    })
+    // console.log("Previous");
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    // this.setState({loading: true});
+    // let data = await fetch(url);
+    // let parsedData = await data.json()
+    // console.log(parsedData);
+    // this.setState({
+    //   page: this.state.page - 1,
+    //   articles: parsedData.articles,
+    //   loading: false
+    // })
+    this.setState({page: this.state.page - 1});
+    this.updateNews();
   }
 
    handleNextClick= async ()=>{
-      console.log("Next");
-      if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-      this.setState({loading: true});
-      let data = await fetch(url);
-      let parsedData = await data.json()
-      this.setState({articles:parsedData.articles})
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading: false
-      })
-    }
+    //   console.log("Next");
+    //   if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
+    //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2eb16594e94245639a9d727016f7dfa5&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    //   this.setState({loading: true});
+    //   let data = await fetch(url);
+    //   let parsedData = await data.json()
+    //   this.setState({articles:parsedData.articles})
+    //   this.setState({
+    //     page: this.state.page + 1,
+    //     articles: parsedData.articles,
+    //     loading: false
+    //   })
+    // }
+    this.setState({page: this.state.page + 1})
+    this.updateNews();
   }
     
     render() {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,12 +6,13 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left : '90%', zIndex: '1'}}> {source} </span>
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left : '90%', zIndex: '1'}}> {source} </span>
         <img src={!imageUrl?"https://www.bing.com/images/search?view=detailv2&form=SBIHVR&iss=sbi&q=imgurl:https%3A%2F%2Fstatic.toiimg.com%2Fthumb%2Fmsid-103291540%2Cimgsize-64350%2Cwidth-400%2Cresizemode-4%2F103291540.jpg&pageurl=https%3A%2F%2Ftimesofindia.indiatimes.com%2Fsports%2Fcricket%2Fasia-cup%2Findia-vs-pakistan-asia-cup-virat-kohli-vs-shaheen-afridi-and-other-player-battles-to-watch-out-for%2Farticleshow%2F103291344.cms&pagetl=India+vs+Pakistan%2C+Asia+Cup%3A+Virat+Kohli+vs+Shaheen+Afridi+and+other+player+battles+to+watch+out+for+%7C+Cricket+News+-+Times+of+India&imgalt=India+vs+Pakistan%2C+Asia+Cup%3A+Virat+Kohli+vs+Shaheen+Afridi+and+other+player+battles+to+watch+out+for&imgsz=195x111&selectedindex=0&id=https%3A%2F%2Fstatic.toiimg.com%2Fthumb%2Fmsid-103291540%2Cimgsize-64350%2Cwidth-400%2Cresizemode-4%2F103291540.jpg&ccid=qOifeYqP&simid=6955629249185&ck=51F8C6667EB2D065E658309EC65FEFE8&thid=OIF.UfjGZn6y0GXmWDCexl%2Fv6A&mediaurl=https%3A%2F%2Fstatic.toiimg.com%2Fthumb%2Fmsid-103291540%2Cimgsize-64350%2Cwidth-400%2Cresizemode-4%2F103291540.jpg&exph=225&expw=400&vt=2&sim=1":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title} </h5>
           <p className="card-text"><small className="text-muted">By {!author? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
-          <p className="card-text">{description}...</p>
+          <p className="card-text">{description}</p>
+
           <a rel = "noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
         </div>
 </div>
